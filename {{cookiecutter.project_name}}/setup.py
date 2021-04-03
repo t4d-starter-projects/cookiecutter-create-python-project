9a2a39cce6fbd69c617a13c41b895643d777368e
Eric Greene <eric@t4d.io>
2021-04-03 15:06:54 -0400

    upgrade pipeline for package deployment

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -3,12 +3,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="{{cookiecutter.project_name}}",
+    name="{{cookiecutter.package_name}}",
     version="0.1.0",
     url="{{cookiecutter.author_url}}",
     license="MIT",
     author="{{cookiecutter.author_name}}",
     author_email="{{cookiecutter.author_email}}",
-    description="{{cookiecutter.project_desc}}",
+    description="{{cookiecutter.package_desc}}",
     packages=find_packages(exclude="src.tests")
 )
